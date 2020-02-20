6b72bebd229ef073386858836b07917f02d2d94d
aakashDeveloper <ahanda205@gmail.com>
2020-02-20 02:30:13 +0000

    a

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Header from './Header';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Functional
+const Home = () => {
+    return(
+        <div>
+            <Header/>
+            <h1>This is React App with Nareshit</h1>
+            <h2>This is about our class</h2>
+            <p>lorem ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. it has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. it was popularised in the 1960s with the release of letraset sheets containing lorem ipsum passages, and more recently with desktop publishing software like aldus pagemaker including versions of lorem ipsum</p>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Home/>,document.getElementById('root'));
