c535742ab1579e106ce5add7877452831950a58e
aakashDeveloper <ahanda205@gmail.com>
2020-03-16 02:31:11 +0000

    a

---
+++ redux2/src/container/Home.js
@@ -0,0 +1,17 @@
+import React, {Component, Fragment} from 'react';
+
+
+class Home extends Component {
+    render(){
+        return(
+            <Fragment>
+                <h1>Home </h1>
+            </Fragment>
+        )
+    }
+}
+
+
+export default Home
+
+
